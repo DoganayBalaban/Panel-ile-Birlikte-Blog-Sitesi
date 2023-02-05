813cfcc9d3e9167a6b68db9a594a4236c35b9c91
Doğanay Balaban <dbalaban1907@gmail.com>
2023-02-05 22:36:13 +0300

    11. ders sonu

--- config/Routes.go
+++ config/Routes.go
@@ -15,7 +15,7 @@ func Routes() *httprouter.Router {
 	r.POST("/admin/add", admin.Dashboard{}.Add)
 	r.GET("/admin/delete/:id", admin.Dashboard{}.Delete)
 	r.GET("/admin/edit/:id", admin.Dashboard{}.Edit)
-
+	r.POST("/admin/update/:id", admin.Dashboard{}.Update)
 	//SERVE FILES
 	r.ServeFiles("/admin/assets/*filepath", http.Dir("admin/assets"))
 	r.ServeFiles("/uploads/*filepath", http.Dir("uploads"))
